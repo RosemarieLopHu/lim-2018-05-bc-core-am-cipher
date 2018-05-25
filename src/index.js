645b8e999c5f6cc3f6dc0116290bffafbf4df34f
RosemarieLopezH <rosemarie61@gmail.com>
2018-05-25 10:46:15 -0500

    Añadí código

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-function encode() {
-    let decoded = document.querySelector('#decoded').value.toLocaleUpperCase() || "Hola";
-    let encoded= document.querySelector('#encoded');
+function clearBoxes() {
+    document.querySelector('#decrypted').value = "";
+    document.querySelector('#encrypted').value = "";
+    document.getElementById("chains").value = "";
+    document.getElementById("offset").value = 0;
+function encrypt() {
+    let decrypted = document.querySelector('#decrypted').value.toLocaleUpperCase() || "";
+    let encrypted = document.querySelector('#encrypted');
     let offset = document.querySelector('#offset');
     let cipherArray = new Array();
-    [...decoded].forEach(char => {
+
+    [...decrypt].forEach(char => {
         //console.log(char.charCodeAt())
+
         if (char.charCodeAt() === 32) {
             cipherArray.push(32);
         } else {
@@ -12,28 +19,6 @@ function encode() {
             //console.log(charNumber)
             cipherChar = (charNumber + parseInt(key.value)) % 26;
             //console.log(cipherChar)
-            cipherArray.push(cipherChar + 65);
-        }
-    })
-    document.getElementById("chains").value = String.fromCharCode(...cipherArray);
-    //console.log(String.fromCharCode(...cipherArray))
-}
-function decode() {
-    let decoded = document.querySelector('#decode');
-    let encoded = document.querySelector('#encode').value.toLocaleUpperCase();
-    let offset = document.querySelector('#offset');
-    let cipherArray = new Array();
-    [...encoded].forEach(char => {
-        //console.log(char.charCodeAt())
-        if (char.charCodeAt() === 32) {
-            cipherArray.push(32);
-        } else {
-            charNumber = char.charCodeAt() - 65;
-            //console.log(charNumber)
-            cipherChar = (charNumber - parseInt(key.value));
-            //console.log(cipherChar)
-            if (cipherChar > 25 || cipherChar < 0) cipherChar = cipherChar + 26
-            cipherChar = cipherChar % 26
             cipherArray.push(cipherChar + 65);
         }
     })
