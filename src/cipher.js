7773961f58def7e5ed2c3ab9d11615bcf6c5c792
RosemarieLopezH <rosemarie61@gmail.com>
2018-06-04 10:15:36 -0500

    actualizacion

--- src/cipher.js
+++ src/cipher.js
@@ -1,48 +1,56 @@
 window.cipher = {
-  encode: function(offset,string){
-  string = string1.value.toUpperCase();
-  offset = parseInt(offsetNumber.value);
-  
-  let cipherArray = [];
-  
-  for(let i = 0; i<string.length; i++){
-     let c_firstAsciiCode = string.charCodeAt(i);
-     let c_secondAsciiCode;
-     let c_newString;
-    if(c_firstAsciiCode >= 65 && c_firstAsciiCode <= 90){
-      c_secondAsciiCode = (c_firstAsciiCode - 65 + offset) % 26 + 65;
-      c_newString = String.fromCharCode(c_secondAsciiCode);
-      cipherArray.push(c_newString);
-      } else if(c_firstAsciiCode = 32){
-      c_secondAsciiCode = c_firstAsciiCode;
-      c_newString = String.fromCharCode(c_secondAsciiCode);
-      cipherArray.push(c_newString);
-     }     
-  }
-  return result1.innerHTML = cipherArray.join('')
+  // Declaro los parámetros de encode (STR porque string es una keyword)
+  encode: (str, offset) => {
+    let result = '';
+    // Ciclo for que recorre el string
+    for (let i = 0; i < str.length; i++) {
+      let character = str[i];
+      if (character.match(/[a-z]/i)) {
+        let code = str.charCodeAt(i);
+        if ((code >= 65) && (code <= 90)) {
+          character = String.fromCharCode(((code - 65 + parseInt(offset)) % 26) + 65);
+        }
+        if ((code >= 97) && (code <= 122)) {
+          character = String.fromCharCode(((code - 97 + parseInt(offset)) % 26) + 97);
+        }
+      }
+      // Ahora se concatena el resultado en result
+      result += character;
+    }
+    // El método encode retorna result luego de toda la iteración y concatenación lista.
+    return result;
   },
 
+  // Declaro los parámetros de encode (STR porque string es una keyword)
+  decode: (str, offset) => {
+    // Declaro un string vacío donde se concatenarán en cada iteración los caracteres descifrados
+    let resultDecode = '';
+    for (let i = 0; i < str.length; i++) {
+      let character = str[i];
+      if (character.match(/[a-z]/i)) {
+        let code = str.charCodeAt(i);
+        if ((code >= 65) && (code <= 90)) {
+          character = String.fromCharCode(((code - 65 - parseInt(offset) + 52) % 26) + 65);
+        }
+        if ((code >= 97) && (code <= 122)) {
+          character = String.fromCharCode(((code - 97 - parseInt(offset) + 52) % 26) + 97);
+        }
+      }
+      // Finalmente concatena el resultado en resultDecode
+      resultDecode += character;
+    }
+    // El método decode retorna resultDecode luego de toda la iteración y concatenación lista.
+    return resultDecode;
+  },
 
-  decode: function(offset,string){
-  string = string1.value.toUpperCase();
-  offset = parseInt(offsetNumber.value);
-
-  let decipherArray = [];
-  
-  for(let i = 0; i<string.length; i++){
-  
-     let d_firstAsciiCode = string.charCodeAt(i);
-     let d_secondAsciiCode;
-     let d_newString;
-     if(d_firstAsciiCode >= 65 && d_firstAsciiCode <= 90){
-        d_secondAsciiCode = (d_firstAsciiCode+65 - offset) % 26 + 65;
-        d_newString = String.fromCharCode(d_secondAsciiCode);
-        decipherArray.push(d_newString);
-     } else if(d_firstAsciiCode = 32){
-        d_secondAsciiCode = d_firstAsciiCode;
-        d_newString = String.fromCharCode(d_secondAsciiCode);
-        decipherArray.push(d_newString);
-     }
+  createCipherWithOffset : (offset) => {
+    return{
+      encode: (string) => {
+        return cipher.encode(string, offset)
+      },
+      decode: (string) => {
+        return cipher.decode(string, offset)
+      }
+    }
   }
-      return result2.innerHTML = decipherArray.join('')
-  }}
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-let string1 = document.getElementById("textToCipherAndDecipher");
-let offsetNumber = document.getElementById("number");
-  //first obtener el input text y  button a través de dos variables diferentes
+// Guardo en variables todos los nodos de mi documento que necesito para hacer manipulación del DOM
+const btnEncode = document.getElementById('btn-encode');
+const btnDecode = document.getElementById('btn-decode');
+const inputMessage = document.getElementById('message');
+const inputNumber = document.getElementById('number');
+const result = document.getElementById('result-paragraph');
 
-let button1 = document.getElementById("cipherButton");
-let button2 = document.getElementById("decipherButton");
+// Evento click para el botón Encode
+btnEncode.addEventListener('click', (event) => {
+  // preventDefault() para detener una acción por defecto, en este caso la del botón dentro de un form
+  event.preventDefault();
+  /* 
+  Se pasa por parámetro el string y offset que el usuario ingresó, además escribe el resultado del método encode en el nodo html indicado
+  */
+  result.innerHTML = cipher.encode(inputMessage.value, inputNumber.value);
+});
 
-//Now obtener el id para la answer del mensaje cifrado y descifrado
-
-let result1 = document.getElementById("cipherResult");
-button1.addEventListener("click", cipher.encode); 
-let result2 = document.getElementById("decipherResult")
-button2.addEventListener("click", cipher.decode); 
+// Evento click para el botón Decode
+btnDecode.addEventListener('click', (event) => {
+  // preventDefault() para detener una acción por defecto, en este caso la del botón dentro de un form
+  event.preventDefault();
+  /* 
+  Se pasa por parámetro el string y offset que el usuario ingresó, además escribe el resultado del método decode en el nodo html indicado
+  */
+  result.innerHTML = cipher.decode(inputMessage.value, inputNumber.value);
+});
